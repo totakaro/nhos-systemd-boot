f99275d1bc03e153259ba7706979da2e8b08de06
totakaro <30659361+totakaro@users.noreply.github.com>
2021-06-19 20:17:48 -0500

    Use systemd-boot.zip file to check installation

--- scripts.sh/systemd.sh
+++ scripts.sh/systemd.sh
@@ -5,17 +5,16 @@
 # MIT License
 
 # Check if SystemD Boot was already installed
-if [ -f /mnt/nhos/scripts.sh/systemd-boot.log ]; then
+if [ -f /mnt/nhos/scripts.sh/systemd-boot.zip ]; then
   exit
 fi
 
 # Downloading SystemD Boot files
-cd /tmp
+cd /mnt/nhos/scripts.sh
 wget -O systemd-boot.zip https://github.com/totakaro/nhos-systemd-boot/blob/main/systemd-boot.zip?raw=true
 unzip systemd-boot.zip
 sudo mkdir /mnt/root
 sudo mount /dev/sda3 /mnt/root
 sudo cp -vr EFI loader /mnt/root
 sudo umount /mnt/root
-sudo touch /mnt/nhos/scripts.sh/systemd-boot.log
 sudo reboot
